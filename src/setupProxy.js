9c4e8718dc1481437020fc90dece7cfd0e697742
adao <adaodcoelho@gmail.com>
2020-12-05 16:55:57 +0000

    first commit

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+/// teste para chamar todos os pedidos da api //
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
+};
