650f05fe5598e440cdfd122dc652b5e07d404ce8
adao <adaodcoelho@gmail.com>
2020-12-10 14:41:26 +0000

    last update version-final-demo

--- src/components/pages/LoginSuccess.js
+++ src/components/pages/LoginSuccess.js
@@ -3,7 +3,7 @@ import React from 'react';
 function LoginSuccess() {
     return (
       <div style= {{textAlign: 'center '}}>
-        <h1>Thanks for loggin </h1>
+        <h1>Thanks for loggin 'adoniscoelhonis'</h1>
         <h4>Now you can talk to Toby Hawk, your personal skater bot.</h4>
 
       </div>
