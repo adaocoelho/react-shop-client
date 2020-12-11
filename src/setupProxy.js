9f4cf47dadaa1e9426ea1aeff348368f314ed83e
adao <adaodcoelho@gmail.com>
2020-12-11 11:27:56 +0000

    final version

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,3 +5,4 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
 };
+
